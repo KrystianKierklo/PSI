8c0cf76c0dbd72dc5fb91360ba0f1aeba942886a
KrystianKierklo <73948612+KrystianKierklo@users.noreply.github.com>
2023-01-24 11:43:53 +0100

    Update settings.py

--- Project/carParking/carParking/settings.py
+++ Project/carParking/carParking/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dbucqd25l)dc$5ov4kg01br-7nqx$bd@fb#17ypqs$t#_p6pa4'
+SECRET_KEY = '*******************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
